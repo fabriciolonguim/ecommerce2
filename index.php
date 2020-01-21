d052a6df33ad082393acfb9ffa0914461e80226e
fabriciolonguim <fblong.longuim657@gmail.com>
2020-01-21 20:41:10 -0300

    criando o pageadmin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 $app = new \Slim\Slim();
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app->config('debug', true);
 
@@ -13,6 +14,15 @@ $app->get('/', function() {
 	echo "OK";
 	*/
 	$page = new Page();
+
+	$page->setTpl("index");
+	
+
+});
+
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
 
 	$page->setTpl("index");
 	
